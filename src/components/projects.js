505cb6a0f49083ff03cda3ced64a0c6ecb395a08
smeg24 <meghanishaaz@gmail.com>
2025-10-11 15:05:24 -0700

    m

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import './style/projects.css'; 
+import demo from "../pollstream.mp4"
 
 function Project() { 
     const stream = ` 
@@ -11,10 +12,18 @@ function Project() {
         <div class="card">  
             <h1 class="header">POLL STREAM</h1>
             <span class="stream">{stream}</span>
-            <h1 class="demo">DEMO</h1>
+            <h1 class="demo2">DEMO</h1>
+            <div class="videowrap">  
+                <video class="demo" controls playsInline poster preload="metadata">  
+                <source src={demo} type="video/mp4"/> 
+                </video>
+            </div>
         </div>
         <div class="card1">  
               <h1 class="store">PassStore</h1>
+              <video class="demo1" controls playsInline poster="" preload="metadata">   
+                <source type="video/mp4"/>
+              </video>
         </div>
     </div>
   );
